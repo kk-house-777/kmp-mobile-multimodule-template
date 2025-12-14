8672e1ec77666a205f83a75f0b961f2012818bd3
kk-house-777 <intelinside.7575@gmail.com>
2025-12-14 20:10:17 +0900

    release

--- kmp-mobile-multimodule-template.rb
+++ kmp-mobile-multimodule-template.rb
@@ -1,9 +1,9 @@
 class KmpMobileMultimoduleTemplate < Formula
   desc "KMP + Tuist multi-module project template generator"
   homepage "https://github.com/kk-house-777/kmp-mobile-tuist-template"
-  url "https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.1.tar.gz"
-  # 例 : curl -L https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.1.tar.gz | shasum -a 256
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.2.tar.gz"
+  # 例 : curl -L https://github.com/kk-house-777/kmp-mobile-tuist-template/archive/refs/tags/v0.0.2.tar.gz | shasum -a 256
+  sha256 "f436d5af0aff4ea9d1ce11efd452c68eccadad6dbed8aeaed9200e035b8d9250"
   license "MIT"
 
   depends_on "cookiecutter"
